17166c7688cf1d4e368a7d18cf37f2c97479e71e
Cristian <balzacode.desarrollo@gmail.com>
2025-11-27 15:26:36 -0400

    minor changues

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import "./App.css";
+import { Outlet } from 'react-router-dom';
 // 1. Importamoos el nuevo componente del formulario
 import AdminDashboard from "./components/AdminDashboard";
 import CustomerDashboard from "./components/CustomerDashboard";
@@ -11,29 +11,19 @@ function App() {
   // 1. Estado para guardad la lista de catalinas
   const { token, user, logout } = useContext(AuthContext);
 
-  const renderDashboard = () => {
-    if (!user) return <Loginpage />;
-
-    if(user.role === 'admin'){
-      return <AdminDashboard /> // Vista de admin
-    } else {
-      return <CustomerDashboard /> // Vista del cliente
-    }
-  };
-
   return (
     <>
       <div className="app-container">
       {token && (
         <div className="header">
          {/* Saluda al usuario por su nombre */}
-          <h1>Panel de {user.role === 'admin' ? 'Administrador' : 'Cliente'} ({user.nombre})</h1>
+          <h1>Panel de {user.role === 'admin' ? 'Administrador' : 'Cliente'} ({user?.nombre})</h1>
           <button onClick={logout}>Cerrar Sesión</button>
         </div>
       )}
 
-      {/* Si no hay token, muestra el Login. Si hay, decide qué panel mostrar. */}
-      {token ? renderDashboard() : <Loginpage />}
+      {/* React Router pintará las rutas hijas aquí */}
+      <Outlet />
     </div>
     </>
   );
