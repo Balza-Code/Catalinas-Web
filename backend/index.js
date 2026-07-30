5d3e9fad1f1ef77130cb1c59f4758876b5ff50a9
Cristian <balzacode.desarrollo@gmail.com>
2026-07-30 00:00:39 -0400

    fix bot controller logic

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 4000;
 // ---Nuevos Cambios---
 // 1. Middelware para que express entienda JSON
 // Esto es muy imporatne. permite que tu servidor reciba datos JSON en las peticiones
-app.use(express.json(limit: '10mb'));
+app.use(express.json({limit: '10mb'}));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // 2. Importar las rutas
